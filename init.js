fb1831f3534581b8ebf08bdebc6265a55f198f1b
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2021-10-14 22:36:13 -0300

    Add files via upload

--- init.js
+++ init.js
@@ -1,28 +1,58 @@
-(function () {
-  function fnAddButtons() {
+function fnAddButtons() {    
+  var div0 = document.createElement("div");
+  div0.className = "filter-forms";
 
-    var div0 = document.createElement("div");
-    div0.className = "filter-forms left";
+  var div1 = document.createElement("div");
+  div1.id = "action_buttons";
+  div1.className = "action_buttons right";
+
+  var btn = document.createElement("button");
+  btn.value = "acknowledge.edit";
+  btn.className = "btn-link";
+  btn.textContent = "Bulk acknowledge"; 
+  btn.type = "submit";
+  btn.name = "action";
+
+  div1.appendChild(btn);
+  div0.appendChild(div1);
+
+  var table = document.getElementsByClassName("list-table")[0];
+  var form = table.parentElement;
 
 
-    var div1 = document.createElement("div");
-    div1.id = "action_buttons";
-    div1.className = "action_buttons";
+  form.prepend(div0);
+}
 
-    var btn = document.createElement("button");
-    btn.value = "acknowledge.edit";
-    btn.className = "btn-alt";
-    btn.textContent = "Bulk acknowledge"; 
-    btn.type = "submit";
-    btn.name = "action";
-
-    div1.appendChild(btn);
-    div0.appendChild(div1);
-
-    document.querySelector("#flickerfreescreen_problem > form").prepend(div0);
+function callback(){
+  if (document.querySelector("#flickerfreescreen_problem > form > div.filter-forms") === null){
+    fnAddButtons();
+    //console.log('Mutation Ocurred!')
   }
-  
+}
 
-  fnAddButtons();
-})();
+function observe(){
+  if (document.querySelector("#flickerfreescreen_problem > form > div.filter-forms") === null){
+    fnAddButtons();
+  }
+
+  const observer = new MutationObserver(mutations => {
+    //console.log(mutations);
+    callback();
+  });
+
+  var article = document.querySelector("body > div.article");
+
+  observer.observe(article, {
+    attributes: true,
+    childList: true,
+    characterData: true,
+    subtree: true
+  });
+}
+
+document.querySelector("body > div.article > script:nth-child(8)").remove
+
+observe();
+
+
 
